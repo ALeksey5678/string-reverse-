c17d312726562e236509c458e2735d49aac174b4
Aleksey5678 <foritlessonsalex@gmail.com>
2022-07-29 13:45:49 +0300

    1)name for parameter "s" was change to "string" 2)name instead of generic "result" is "reverseString" 3)renamed variable "i" to "numOfSymbols" 4)decomposing a class is still in progress

--- src/com/company/reverse/Reverse.java
+++ src/com/company/reverse/Reverse.java
@@ -7,15 +7,15 @@ public class Reverse {
         System.out.println(reverse("ad12fgiu57"));
     }
 
-    private static String reverse(String s) {
-        StringBuilder result = new StringBuilder(s.replaceAll("[\\d+]", "")).reverse();
+    private static String reverse(String string) {
+        StringBuilder reverseString = new StringBuilder(string.replaceAll("[\\d+]", "")).reverse();
 
-        for (int i = 0; i < s.length(); i++) {
-            if (!Character.isLetter(s.charAt(i))) {
-                result.insert(i, s.charAt(i));
+        for (int numOfSymbols   = 0; numOfSymbols < string.length(); numOfSymbols++) {
+            if (!Character.isLetter(string.charAt(numOfSymbols))) {
+                reverseString.insert(numOfSymbols, string.charAt(numOfSymbols));
             }
         }
-        return result.toString();
+        return reverseString.toString();
 
     }
 }
