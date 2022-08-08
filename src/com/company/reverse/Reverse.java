f81ea598d181e2f6952e83b5dae45e7ca3f1a177
Aleksey5678 <foritlessonsalex@gmail.com>
2022-08-08 16:28:53 +0300

    Add some changes(rename literal)

--- src/com/company/reverse/Reverse.java
+++ src/com/company/reverse/Reverse.java
@@ -3,10 +3,10 @@ package com.company.reverse;
 
 public class Reverse {
     private static final String MATCH_ALL_DIGITS = "[\\d+]";
-    private static final String STRING_REGEX = "";
+    private static final String EMPTY = "";
 
     public String ActionOfReversing(String stringToReverse) {
-        StringBuilder reverseString = new StringBuilder(stringToReverse.replaceAll(MATCH_ALL_DIGITS, STRING_REGEX)).reverse();
+        StringBuilder reverseString = new StringBuilder(stringToReverse.replaceAll(MATCH_ALL_DIGITS, EMPTY)).reverse();
         for (int indexOfSymbols = 0; indexOfSymbols < stringToReverse.length(); indexOfSymbols++) {
             if (!Character.isLetter(stringToReverse.charAt(indexOfSymbols))) {
                 reverseString.insert(indexOfSymbols, stringToReverse.charAt(indexOfSymbols));
