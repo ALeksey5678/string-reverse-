bbdc599cca9b7e2904b86d0c69eee796483ca10f
Aleksey5678 <foritlessonsalex@gmail.com>
2022-08-11 19:28:20 +0300

    rename method name (getReversed)

--- src/com/company/reverse/Application.java
+++ src/com/company/reverse/Application.java
@@ -3,7 +3,7 @@ package com.company.reverse;
 public class Application {
     public static void main(String[] args) {
         Reverse reverse = new Reverse();
-        System.out.println(reverse.ActionOfReversing("asdfk124gjh56"));
+        System.out.println(reverse.getReversed("asdfk124gjh56"));
     }
 }
 
--- src/com/company/reverse/Reverse.java
+++ src/com/company/reverse/Reverse.java
@@ -5,7 +5,7 @@ public class Reverse {
     private static final String MATCH_ALL_DIGITS = "[\\d+]";
     private static final String EMPTY = "";
 
-    public String ActionOfReversing(String stringToReverse) {
+    public String getReversed(String stringToReverse) {
         StringBuilder reverseString = new StringBuilder(stringToReverse.replaceAll(MATCH_ALL_DIGITS, EMPTY)).reverse();
         for (int indexOfSymbols = 0; indexOfSymbols < stringToReverse.length(); indexOfSymbols++) {
             if (!Character.isLetter(stringToReverse.charAt(indexOfSymbols))) {
