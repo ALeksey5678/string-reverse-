fd0726d7789c1d2a397f8782d3b2c6eb906e4d84
Aleksey5678 <foritlessonsalex@gmail.com>
2022-08-14 12:09:27 +0300

    Add @BeforeAll to optimize code

--- src/com/company/reverse/ReverseTest.java
+++ src/com/company/reverse/ReverseTest.java
@@ -1,12 +1,19 @@
 package com.company.reverse;
 
+import org.junit.jupiter.api.BeforeAll;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReverseTest {
+   static Reverse reverse;
+
+    @BeforeAll
+   static void prepeareData() {
+        reverse = new Reverse();
+    }
 
     @org.junit.jupiter.api.Test
     void testGetReversedStringMethod() {
-        Reverse reverse=new Reverse();
-        assertEquals("dcba1",reverse.getReversed("abcd1"));
+        assertEquals("dcba1", reverse.getReversed("abcd1"));
     }
 }
